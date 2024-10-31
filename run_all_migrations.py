36061ad97db6dd2e22fdfcdd74d364927f9f8c6f
Abdulbasit Alabi <abdulbasitdamilola6@gmail.com>
2024-10-31 16:42:13 +0100

    ud..

--- run_all_migrations.py
+++ run_all_migrations.py
@@ -1,28 +1,25 @@
 import subprocess
 
 app_labels = ["accounting", "admin", "auth", "companies", "content_types", "hr", "inventory", "project_costing", "purchase", "registration", "sales", "users"]
-# app_label = 'your_app_name'  # Replace with your app name
 
 # Get a list of all migrations
-# Run a for loop to go through all the apps
-results = []
-for app_label in app_labels:
-	result_migrations = []
-	result = subprocess.run(
-    		["python", "manage.py", "showmigrations", app_label],
-    		capture_output=True, text=True
-	)
-	result_migrations.append(result)
-	results.append(result_migrations)
-migrations = []
-for result in results:
-	migrations.append([
-    		line.strip().split(" ")[-1]
-   		for line in result.stdout.splitlines()
-    		if line.strip() and not line.startswith(" [X]")
-	])
+migrations = {}
 
-# Run sqlmigrate for each migration
-for migration in migrations:
-    print(f"\n--- SQL for migration {migration} ---")
-    subprocess.run(["python", "manage.py", "sqlmigrate", app_label, migration])
+for app_label in app_labels:
+    result = subprocess.run(
+        ["python", "manage.py", "showmigrations", app_label],
+        capture_output=True, text=True
+    )
+
+    # Parse the output to get the list of migrations that have not been applied
+    migrations[app_label] = [
+        line.strip().split(" ")[-1]
+        for line in result.stdout.splitlines()
+        if line.strip() and not line.startswith("[X]")
+    ]
+
+# Run sqlmigrate for each migration in each app
+for app_label, app_migrations in migrations.items():
+    for migration in app_migrations:
+        print(f"\n--- SQL for migration {migration} in app {app_label} ---")
+        subprocess.run(["python", "manage.py", "sqlmigrate", app_label, migration])
