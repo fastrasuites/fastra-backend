6fcbe9a2d36b8c6b8ff9608d1b6b5ca7daaecf01
seyifash <fasugbeoluwaseyi@gmail.com>
2025-02-07 09:55:22 +0100

    updating branch with existing code from seyi branch

--- core/settings.py
+++ core/settings.py
@@ -52,6 +52,7 @@ CORS_ALLOWED_ORIGINS = [
     # frontend
     'https://fastrasuite.com',
     'https://www.fastrasuite.com',
+    'https://app.fastrasuite.com'
 
     # backend
     # 'https://fastrasuiteapi.com.ng',
