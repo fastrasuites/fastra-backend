d6af70528ed5f588ae1e9f173f3553cc31ca2e54
seyifash <fasugbeoluwaseyi@gmail.com>
2025-07-23 10:28:38 +0100

    is_onboarded api is added

--- companies/urls.py
+++ companies/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ChangeAdminPassword, TenantViewSet, VerifyEmail, RequestForgottenPasswordView,\
+from .views import ChangeAdminPassword, MarkOnboardedView, OnboardingStatusView, TenantViewSet, VerifyEmail, RequestForgottenPasswordView,\
     ResendVerificationEmail, UpdateCompanyProfileView, ResendOTPView, ProtectedView
 
 from rest_framework_simplejwt.views import (
@@ -25,6 +25,8 @@ urlpatterns = [
 
     path('update-company-profile/', UpdateCompanyProfileView.as_view(), name='update-company-profile'),
     path('change-admin-password/', ChangeAdminPassword.as_view(), name='change-admin-password'),
+    path('onboarding-status/', OnboardingStatusView.as_view(), name='onboarding_status'),
+    path('mark-onboarded/', MarkOnboardedView.as_view(), name='mark_onboarded'),
 
     path('test/', ProtectedView.as_view())
 
