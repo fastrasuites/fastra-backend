e96dd2c7b382701cf1950877fcd3290cd735bdf7
Adeleke Awwal <59998909+codemaestro02@users.noreply.github.com>
2025-08-16 00:05:08 +0100

    user image when logging in

--- registration/views.py
+++ registration/views.py
@@ -271,9 +271,3 @@ class AccessRightViewSet(SoftDeleteWithModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
-class LoginDetailsView(APIView):
-
-    def get(self, request, *args, **kwargs):
-        pass
-
-
